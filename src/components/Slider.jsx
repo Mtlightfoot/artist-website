fc47aebccad5d31d46c58e6d3b0f2e143266e010
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-08-22 15:24:29 +0100

    Gave slider images a slight black overlay to make text more legible, also position first image to look better on mobile

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,11 +9,13 @@ function Slider() {
     return (
         <Carousel controls={false} indicators={false}>
             <Carousel.Item interval={4000}>
-                <img
-                    className="sliderImg d-block min-vh-90 w-100"
-                    src="HeroSlider-1@2x.jpg"
-                    alt="First slide"
-                />
+                <div className='sliderImgBg sliderImg1'>
+                    <img
+                        className="sliderImg1 sliderImg d-block min-vh-90 w-100"
+                        src="HeroSlider-1@2x.jpg"
+                        alt="First slide"
+                    />
+                </div>
                 <Carousel.Caption className='carouselCaption h-100 align-items-start justify-content-center'>
                     <h3 className='carouselText'>Artist Spotlight</h3>
                     <h4 className='carouselArtist'>{pictures[0].artist}</h4>
@@ -21,22 +23,26 @@ function Slider() {
             </Carousel.Item>
 
             <Carousel.Item interval={2000}>
-                <img
-                    className="sliderImg d-block min-vh-90 w-100"
-                    src="HeroSlider-2@2x.jpg"
-                    alt="Second slide"
-                />
+                <div className='sliderImgBg'>
+                    <img
+                        className="sliderImg d-block min-vh-90 w-100"
+                        src="HeroSlider-2@2x.jpg"
+                        alt="Second slide"
+                    />
+                </div>
                 <Carousel.Caption className='carouselCaption h-100 align-items-start justify-content-center'>
                     <h3 className='carouselText'>Artist Spotlight</h3>
                     <h4 className='carouselArtist'>{pictures[1].artist}</h4>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={2000}>
-                <img
-                    className="sliderImg d-block min-vh-90 w-100"
-                    src="HeroSlider-3@2x.jpg"
-                    alt="Third slide"
-                />
+                <div className='sliderImgBg'>
+                    <img
+                        className="sliderImg d-block min-vh-90 w-100"
+                        src="HeroSlider-3@2x.jpg"
+                        alt="Third slide"
+                    />
+                </div>
                 <Carousel.Caption className='carouselCaption h-100 align-items-start justify-content-center'>
                     <h3 className='carouselText'>Artist Spotlight</h3>
                     <h4 className='carouselArtist'>{pictures[2].artist}</h4>
