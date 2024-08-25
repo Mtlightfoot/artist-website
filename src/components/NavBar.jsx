a3b20fae2c27ec919717d00624d571a268e99af5
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-08-25 09:46:51 +0100

    Changed styling of navbar so it has logo in the middle and search on the right on mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,13 +16,19 @@ function NavBar() {
                         <Nav.Link id='navLink' href="#link">Journal</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-                <Navbar.Brand className='navbar-brand-center mx-0' id='logo' href="#home">ACME Co</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                <Navbar.Brand className='navbar-brand-center mx-0' id='logo' href="#home">ACME Co</Navbar.Brand>
+
+                <Nav.Link id='search navLink' href="#link"> <span id='searchDisappear'>Search</span> <svg id='search' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+                </Nav.Link>
                 <Navbar.Collapse id="rightSideNav basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link id='navLink' href="#home">About</Nav.Link>
                         <Nav.Link id='navLink' href="#link">Enquire</Nav.Link>
-                        <Nav.Link id='navLink' href="#link">Search <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <Nav.Link id='navLink' href="#link"> <span id='searchFull'>Search</span> <svg id='searchFull' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                         </svg>
                         </Nav.Link>
